807763852ea2ab0c1486bc2bc129a5c55f490ab3
Jayson Malabanan <jmalab24+github@gmail.com>
2024-09-29 01:05:47 -0400

    build updates

--- app/src/components/Categories/Category/index.tsx
+++ app/src/components/Categories/Category/index.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import { Card, CardBody, Typography, Button } from '@material-tailwind/react';
 import {
   PencilSquareIcon,
--- app/vite.config.js
+++ app/vite.config.js
@@ -5,4 +5,23 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   define: { 'process.env': {} },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // Split vendor dependencies into separate chunks
+          if (id.includes('node_modules')) {
+            return id.split('node_modules/')[1].split('/')[0]; // Creates a chunk for each module
+          }
+        },
+      },
+    },
+    minify: 'terser', // You can specify 'esbuild' or 'terser'
+    terserOptions: {
+      compress: {
+        drop_console: true, // Remove console logs in production
+      },
+      mangle: true, // Enable variable name mangling
+    },
+  },
 });
