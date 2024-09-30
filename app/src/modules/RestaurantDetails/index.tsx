d46970e194613319d5c5f57d56b1df92df8d1d5e
Jayson Malabanan <jmalab24+github@gmail.com>
2024-09-30 17:12:50 -0400

    move restaurant to file

--- app/src/modules/RestaurantDetails/index.tsx
+++ app/src/modules/RestaurantDetails/index.tsx
@@ -1,65 +1,8 @@
 import MapAndTable from '../MapAndTable';
-
-const Restaurants = [
-  {
-    name: 'Takumi',
-    type: 'Japanese',
-    distance: '1.2 miles',
-  },
-  {
-    name: 'Takumi 1',
-    type: 'Japanese',
-    distance: '1.8 miles',
-  },
-  {
-    name: 'Takumi 2',
-    type: 'Japanese',
-    distance: '0.8 miles',
-  },
-  {
-    name: 'Takumi 3',
-    type: 'Japanese',
-    distance: '3 miles',
-  },
-  {
-    name: 'Takumi 4',
-    type: 'Japanese',
-    distance: '3 miles',
-  },
-  {
-    name: 'Takumi 5',
-    type: 'Japanese',
-    distance: '5 miles',
-  },
-  {
-    name: 'Takumi 6',
-    type: 'Japanese',
-    distance: '5 miles',
-  },
-  {
-    name: 'Takumi 7',
-    type: 'Japanese',
-    distance: '5 miles',
-  },
-  {
-    name: 'Takumi 8',
-    type: 'Japanese',
-    distance: '5 miles',
-  },
-  {
-    name: 'Takumi 9',
-    type: 'Japanese',
-    distance: '5 miles',
-  },
-  {
-    name: 'Takumi 10',
-    type: 'Japanese',
-    distance: '5 miles',
-  },
-];
+import restaurants from './restaurants.json';
 
 export const RestaurantDetails = () => {
-  return <MapAndTable records={Restaurants} title="Restaurants" />;
+  return <MapAndTable records={restaurants} title="Restaurants" />;
 };
 
 export default RestaurantDetails;
