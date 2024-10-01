f0329bc4450cfc08dc727e7bee725eaa7aae7faa
Jayson Malabanan <jmalab24+github@gmail.com>
2024-10-01 00:45:20 -0400

    fix

--- app/src/modules/Navbar/index.tsx
+++ app/src/modules/Navbar/index.tsx
@@ -71,10 +71,9 @@ export const Navbar = () => {
             <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
           </svg>
         </label>
-
-        <ul className="menu menu-horizontal px-1 hidden lg:visible">
+        <ul className="menu menu-horizontal px-1">
           {NAV_MENU.map(({ name, href }) => (
-            <li>
+            <li className="hidden lg:block">
               <a href={href}>{name}</a>
             </li>
           ))}
