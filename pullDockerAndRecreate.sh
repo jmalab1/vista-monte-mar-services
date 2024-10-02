ab4fb45581e4f18706cef0cc4ed5a3091c3682ac
Jayson Malabanan <jmalab24+github@gmail.com>
2024-10-02 01:04:19 -0400

    updates

--- pullDockerAndRecreate.sh
+++ pullDockerAndRecreate.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-docker pull jmalab24/costa-rica-condo-app:latest
+docker pull jmalab24/vista-monte-mar-app:latest
+docker pull jmalab24/vista-monte-mar-be:latest
 docker compose -f docker-compose.prod.yml up --build app server --force-recreate -d
