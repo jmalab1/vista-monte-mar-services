b7ac9e4946e216401fb0ff1e207831df41d77a55
Jayson Malabanan <jmalab24@gmail.com>
2024-10-03 00:09:41 -0400

    dev script

---
+++ pullDockerAndRecreate.dev.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+docker pull jmalab24/vista-monte-mar-app:latest
+docker pull jmalab24/vista-monte-mar-be:latest
+docker compose -f docker-compose.dev.yml up --build app server --force-recreate -d
