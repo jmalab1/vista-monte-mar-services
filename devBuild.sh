d4aa55b24b51746c53d8c841ffbd638e8c82eed4
Jayson Malabanan <jmalab24@gmail.com>
2025-02-01 23:33:56 -0500

    do a build with updated stuff

--- devBuild.sh
+++ devBuild.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-docker build -t vista-monte-mar-server -f ../vista-monte-mar-be/Dockerfile ../vista-monte-mar-be
-docker build -t vista-monte-mar-app -f ../vista-monte-mar-app/Dockerfile ../vista-monte-mar-app
+cd ../vista-monte-mar-be; npm install; cd -;
+
+docker build -t vista-monte-mar-server -f ../vista-monte-mar-be/Dockerfile ../vista-monte-mar-be --no-cache
+
+cd ../vista-monte-mar-app; npm install; npm run build; cd -;
+
+docker build -t vista-monte-mar-app -f ../vista-monte-mar-app/Dockerfile ../vista-monte-mar-app --no-cache
 
 docker compose up -d --force-recreate --build
